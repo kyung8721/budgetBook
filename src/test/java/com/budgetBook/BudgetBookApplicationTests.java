680f51f000f3bc8340f7993606e7211a5e02699a
kyung8721 <kyunbae8721@gmail.com>
2024-10-02 19:51:48 +0900

    [배경민]  - budgetBook 프로젝트 생성

---
+++ src/test/java/com/budgetBook/BudgetBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budgetBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
