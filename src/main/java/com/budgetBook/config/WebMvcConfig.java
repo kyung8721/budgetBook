e03b754fae6401f42a7cd7d2ee2183ccd8832e41
desktop <kyunbae8721@gmail.com>
2024-11-03 22:37:19 +0900

    [배경민]  - 로그인이 되어 있으면 로그인 이나 회원가입, 아이디 비밀번호 찾기에 접속 안되게 하기  - 로그인이 안되어 있으면 budgetBook/money로 시작하는 페이지에 접근 못하도록 하기

--- src/main/java/com/budgetBook/config/WebMvcConfig.java
+++ src/main/java/com/budgetBook/config/WebMvcConfig.java
@@ -1,10 +1,12 @@
 package com.budgetBook.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.budgetBook.common.FileManager;
+import com.budgetBook.interceptor.PermissionInterceptor;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer{
@@ -13,5 +15,15 @@ public class WebMvcConfig implements WebMvcConfigurer{
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/images/**")
 		.addResourceLocations("file:///" + FileManager.FILE_UPLOAD_PATH + "/"); // Mac이나 리눅스는 //, 윈도우는 ///
+	}
+	
+	// 인터셉트 설정
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		PermissionInterceptor interceptor = new PermissionInterceptor();
+		
+		registry.addInterceptor(interceptor)
+		.addPathPatterns("/**") // 전체 url이 이 인터셉터를 거치도록 설정
+		.excludePathPatterns("/static/**", "/images/**", "/budgetBook/user/logout"); // 인터셉터를 거치지 않을 일부 url 설정
 	}
 }
---
+++ src/main/java/com/budgetBook/interceptor/PermissionInterceptor.java
@@ -0,0 +1,45 @@
+package com.budgetBook.interceptor;
+
+import java.io.IOException;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+public class PermissionInterceptor implements HandlerInterceptor{
+	@Override
+	public boolean preHandle(
+			HttpServletRequest request
+			, HttpServletResponse response
+			, Object handler) throws IOException {
+		
+		HttpSession session = request.getSession();
+		Integer userId = (Integer)session.getAttribute("userId");
+		
+		// 요청된 url을 받아옴
+		String uri = request.getRequestURI();
+		
+		// 로그인이 안된 상태라면 로그인 화면으로 이동
+		if(userId == null) {
+			if(uri.startsWith("/budgetBook/money/")) {
+				// url이 money로 시작된다면
+				response.sendRedirect("/budgetBook/user/login-view");
+				return false;
+			}else if(uri.startsWith("/budgetBook/user/profile-view")){
+				// url이 user로 시작되는 프로필 수정 페이지라면
+				response.sendRedirect("/budgetBook/user/login-view");
+				return false;
+			}
+		}else {
+			// 로그인된 상태라면 로그인 화면에 접근하지 못 하도록 설정
+			if(uri.startsWith("/budgetBook/user/") && !(uri.startsWith("/budgetBook/user/profile-view"))) {
+				response.sendRedirect("/budgetBook/money/main-view");
+				return false;
+			}
+		}
+		
+		return true;
+	}
+}
